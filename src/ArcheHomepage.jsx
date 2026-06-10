908651f278c7b1cfa6b440406d98ae6c3be286f2
ArchePersona <archepersona@gmail.com>
2026-06-10 14:08:55 -0400

    Add ArcheHomepage component

---
+++ src/ArcheHomepage.jsx
@@ -0,0 +1,8 @@
+export default function ArcheHomepage() {
+  return (
+    <main>
+      <h1>ArchePersona</h1>
+      <p>Behavioral infrastructure for character and consequence.</p>
+    </main>
+  );
+}
